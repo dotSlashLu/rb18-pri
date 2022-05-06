bc77bc27eb98e9c52a8e86d4f29f2768c245ca2b
zhangqiang10 <zhangqiang10@wps.cn>
2022-05-07 02:10:09 +0800

    fix csum issues

--- probes/src/xdp/main.rs
+++ probes/src/xdp/main.rs
@@ -1,13 +1,12 @@
 #![no_std]
 #![no_main]
 
-use core::convert::TryInto;
 use core::mem::{size_of, transmute};
 use memoffset::offset_of;
 use redbpf_probes::xdp::prelude::*;
 
-use probes::xdp::{ClientInfo, LocalInfo};
 use probes::xdp::csum::*;
+use probes::xdp::{ClientInfo, LocalInfo};
 
 program!(0xFFFFFFFE, "GPL");
 
@@ -20,9 +19,65 @@ static mut L2C: HashMap<LocalInfo, ClientInfo> = HashMap::with_max_entries(8);
 #[map(link_section = "maps/dst")]
 static mut DST: LruHashMap<[u16; 5], [u16; 5]> = LruHashMap::with_max_entries(1024);
 
+fn process_eth(ethh: *mut ethhdr, new_src: [u8; 6], new_dst: [u8; 6]) {
+    unsafe {
+        (*ethh).h_source = new_src;
+        (*ethh).h_dest = new_dst;
+    }
+}
+
+fn process_l3(iph: *mut iphdr, new_saddr: u32, new_daddr: u32) {
+    unsafe {
+        (*iph).saddr = new_saddr;
+        (*iph).daddr = new_daddr;
+    }
+
+    // csum
+    unsafe {
+        // _ = printk!("original l3 csum: %x", (*iph).check);
+        (*iph).check = 0;
+    }
+    let mut csum = 0_u64;
+
+    csum_ipv4(iph, &mut csum);
+    // _ = printk!("new l3 csum: %lu -> %x", csum, csum as u16);
+    unsafe {
+        (*iph).check = csum as u16;
+    }
+}
+
+fn process_l4_tcp(tcph: *mut tcphdr, new_src: u16, new_dst: u16) {
+    unsafe {
+        const NWORD: usize = size_of::<tcphdr>() / size_of::<u16>();
+        let old_bytes = transmute::<tcphdr, [u16; NWORD]>(*tcph);
+
+        // _ = printk!("src: %d dest: %d", new_src, new_dst);
+        (*tcph).source = new_src;
+        (*tcph).dest = new_dst;
+
+        //// csum
+        let mut csum = (*tcph).check;
+        let bytes = transmute::<tcphdr, [u16; NWORD]>(*tcph);
+        // _ = printk!("new tcp");
+        // for b in bytes {
+        //     _ = printk!("b: %x", b);
+        // }
+
+        for (i, b) in bytes.iter().enumerate() {
+            if *b != old_bytes[i] {
+                // _ = printk!("bytes %d %x differs from %x", i as i32, *b, old_bytes[i]);
+                csum = csum_ipv4_l4(csum, old_bytes[i], *b);
+            }
+        }
+        // _ = printk!("new_csum %x", csum);
+
+        (*tcph).check = csum;
+    }
+}
+
 #[xdp]
 pub fn kern(ctx: XdpContext) -> XdpResult {
-    let ip = match ctx.ip_mut() {
+    let iph = match ctx.ip_mut() {
         Ok(ip) => ip,
         Err(_) => {
             // printk!("not ip");
@@ -33,35 +88,36 @@ pub fn kern(ctx: XdpContext) -> XdpResult {
     unsafe {
         match ctx.transport_mut() {
             Ok(t) => match t {
-                TransportMut::TCP(tcp) => {
+                TransportMut::TCP(tcph) => {
                     // client
-                    let cip = (*ip).saddr;
-                    let cport = (*tcp).source;
-                    let cdaddr = (*ip).daddr;
-                    let cdport = (*tcp).dest;
+                    let cip = (*iph).saddr;
+                    let cport = (*tcph).source;
+                    let cdaddr = (*iph).daddr;
+                    let cdport = (*tcph).dest;
 
                     // virtual
-                    // let vip = u32::from_be_bytes([127, 0, 0, 42]).to_be(); // 127.0.0.42
-                    let vip = u32::from_be_bytes([10, 13, 148, 51]).to_be(); // 127.0.0.42
+                    // let vip = u32::from_be_bytes([127, 0, 0, 42]).to_be();
+                    let vip = u32::from_be_bytes([10, 13, 148, 51]).to_be();
                     let vport = 42;
 
                     // local
-                    let lip = u32::from_be_bytes([10, 13, 148, 51]).to_be(); // 10.13.148.51
+                    let lip = u32::from_be_bytes([10, 13, 148, 51]).to_be();
+                    // let lport: u16 = u16::from(44444_u16).to_be();
                     let lport: u16 = u16::from(44444_u16).to_be();
 
                     // rs
                     // let rip = u32::from_be_bytes([10, 13, 148, 51]).to_be(); // local
                     let rip = u32::from_be_bytes([10, 13, 5, 33]).to_be(); // vm
-                    let rport = 80_u16.to_be();
+                    let rport = 80_u16.swap_bytes();
 
-                    let client_ip = transmute::<u32, [u8; 4]>((*ip).saddr);
+                    let client_ip = transmute::<u32, [u8; 4]>((*iph).saddr);
                     let mut k: [u16; 5] = [0; 5];
                     for i in 0..4 {
                         k[i] = client_ip[i] as u16
                     }
-                    k[4] = t.source();
+                    k[4] = cport;
 
-                    let dip = transmute::<u32, [u8; 4]>((*ip).daddr);
+                    let dip = transmute::<u32, [u8; 4]>(cdaddr);
                     let mut v: [u16; 5] = [0; 5];
                     for i in 0..4 {
                         v[i] = dip[i] as u16;
@@ -75,9 +131,13 @@ pub fn kern(ctx: XdpContext) -> XdpResult {
                     // }
                     // bpf_trace_printk(&msg[0..5]);
 
-                    if (*ip).daddr == vip && t.dest() == vport {
-                        printk!("hit rule");
-                        let saddr = (*ip).saddr;
+                    if (*iph).daddr == vip && t.dest() == vport {
+                        _ = printk!("hit rule");
+
+                        // _ = printk!("old tcp");
+                        // for b in old_bytes {
+                        //     _ = printk!("b: %x", b);
+                        // }
 
                         let mut localinfo = LocalInfo::new();
                         localinfo.ip = lip;
@@ -85,130 +145,61 @@ pub fn kern(ctx: XdpContext) -> XdpResult {
                         let localinfo = &localinfo;
 
                         let mut clientinfo = ClientInfo::new();
-                        clientinfo.ip = saddr;
-                        clientinfo.port = t.source();
+                        clientinfo.ip = cip;
+                        clientinfo.port = cport;
                         let clientinfo = &clientinfo;
 
                         C2L.set(&clientinfo.clone(), &localinfo.clone());
-                        L2C.set(localinfo, clientinfo);
+                        L2C.set(&localinfo.clone(), &clientinfo.clone());
 
                         // process eth hdr
-                        let mut ethhdr = match ctx.eth_mut() {
+                        let ethh = match ctx.eth_mut() {
                             Ok(ethhdr) => ethhdr,
                             Err(_) => return Ok(XdpAction::Aborted),
                         };
-                        // let mut msg: [u8; 7] = [0; 7];
-                        // for i in 0..6 {
-                        //     msg[i] = (*ethhdr).h_dest[i];
-                        //     printk!("%d", msg[i]);
-                        // }
-                        // msg[6] = b'\0';
-                        // bpf_trace_printk(&msg);
-                        (*ethhdr).h_source = (*ethhdr).h_dest;
-                        // 74:ea:c8:42:f2:01
-                        // what is the endianess?
-                        // (*ethhdr).h_dest = [0x47,0xae,0x8c,0x24,0x2f,0x10];
-                        // (*ethhdr).h_dest = [0x74,0xea,0xc8,0x42,0xf2,0x01];
-                        (*ethhdr).h_dest = [0x0, 0x0, 0x0, 0x0, 0x0, 0x01];
 
-                        (*ip).saddr = lip;
-                        (*ip).daddr = rip;
-
-                        printk!("source: %d", lport);
-                        (*tcp).source = lport;
-                        (*tcp).dest = rport;
-
-                        // l3 csum
-                        printk!("original l3 csum: %x", (*ip).check);
-                        (*ip).check = 0;
-                        let mut csum = 0_u64;
-                        csum_ipv4(ip, &mut csum);
-                        printk!("new l3 csum: %lu -> %x", csum, csum as u16);
-                        (*ip).check = csum as u16;
-
-                        // l4 csum
-                        printk!("original l4 csum: %x", (*tcp).check);
-
-                        // v1
-                        let data_start = tcp as *mut u32;
-                        let data_len = size_of::<tcphdr>();
-                        let mut csum = 0;
-                        csum_ipv4_l4(data_start, data_len, &mut csum, ip);
-                        printk!("new l4 csum: %lu -> %x", csum, csum as u16);
-
-                        // v2
-                        let mut csum = (*tcp).check as u64;
-                        csum = csum_ipv4_l4_2(csum, (*ip).saddr, (*ip).daddr);
-                        csum = csum_ipv4_l4_2(csum, vip, (*ip).daddr); // daddr
-                        csum = csum_ipv4_l4_2(csum, saddr, lip); // saddr
-                        csum = csum_ipv4_l4_2(csum, cport.into(), lport.into()); // sport
-                        csum = csum_ipv4_l4_2(csum, vport.into(), 80_u32.to_be()); // dport
-                        printk!("new l4 csum: %lu -> %x", csum, csum as u16);
-
-                        // v4
-                        let mut csum = (*tcp).check as u32;
-                        csum = csum_ipv4_l4_4(csum, (*ip).saddr, (*ip).daddr);
-                        csum = csum_ipv4_l4_4(csum, vip, (*ip).daddr); // daddr
-                        csum = csum_ipv4_l4_4(csum, saddr, lip); // saddr
-                        csum = csum_ipv4_l4_4(csum, cport.into(), lport.into()); // sport
-                        csum = csum_ipv4_l4_4(csum, vport.into(), 80_u32.to_le()); // dport
-                        printk!("new l4 csum: %u -> %x", csum, csum as u16);
-
-                        // v5
-                        let mut csum = (*tcp).check;
-                        csum = csum_ipv4_l4_5_word(csum, saddr, lip); // saddr
-                        csum = csum_ipv4_l4_5_word(csum, cdaddr, (*ip).daddr); // daddr
-                        csum = csum_ipv4_l4_5(csum, cport, lport); // sport
-                        csum = csum_ipv4_l4_5(csum, vport, rport); // dport
-                        printk!("new l4 csum v5: %x", csum);
-
-                        // v5 - protocol + all to le
-                        let mut csum = (*tcp).check;
-                        csum = csum_ipv4_l4_5_word(csum, saddr.to_le(), lip.to_le()); // saddr
-                        csum = csum_ipv4_l4_5_word(csum, cdaddr.to_le(), (*ip).daddr.to_le()); // daddr
-                        csum = csum_ipv4_l4_5_word(csum, (*ip).protocol.into(), 0x6); // protocol
-                        csum = csum_ipv4_l4_5(csum, cport.to_le(), lport.to_le()); // sport
-                        csum = csum_ipv4_l4_5(csum, vport.to_le(), rport.to_le()); // dport
-                        printk!("new l4 csum v5-protocol + all le: %x", csum);
-
-                        // let mut csum = (*tcp).check.to_le();
-                        // csum = csum_ipv4_l4_5_word(csum, 0, lip); // saddr
-                        // csum = csum_ipv4_l4_5_word(csum, 0, (*ip).daddr); // daddr
-                        // csum = csum_ipv4_l4_5(csum, 0, u16::from((*ip).protocol)); // protocol
-                        // csum = csum_ipv4_l4_5(csum, cport, lport); // sport
-                        // csum = csum_ipv4_l4_5(csum, vport, rport); // dport
-                        // csum = csum.to_be();
-                        // printk!("new l4 csum: %x", csum);
-
-                        let mut csum = csum_ipv4_l4_6((*tcp).check.to_le(), cip.to_le(), (*ip).saddr); // saddr
-                        csum = csum_ipv4_l4_6(csum.to_le(), cdaddr.to_le(), rip); // daddr
-                        csum = csum_ipv4_l4_6(csum.to_le(), u32::from(cport).to_le(), lport.into()); // sport
-                        csum = csum_ipv4_l4_6(csum.to_le(), u32::from(vport).to_le(), rport.into()); // dport
-                        // printk!("csum 32: %x 16: %x", csum, (csum.to_le() >> 16) as u16);
-                        // let csum = ((csum.to_le() >> 16) as u16).to_be();
-                        printk!("new l4 csum: %x", csum);
-
-                        (*tcp).check = csum;
+                        process_eth(ethh, (*ethh).h_dest, [0x0, 0x0, 0x0, 0x0, 0x0, 0x01]);
+                        process_l3(iph, lip, rip);
+                        process_l4_tcp(tcph, lport, rport);
 
                         return Ok(XdpAction::Tx);
                     }
 
-                    if t.dest() == lport {
-                        bpf_trace_printk(b"hit local\0");
+                    // rs -> local
+                    if (*tcph).dest == lport {
+                        _ = printk!("hit local!!");
                         let mut localinfo = LocalInfo::new();
                         localinfo.ip = lip;
                         localinfo.port = lport;
-                        let clientinfo = L2C.get(&localinfo).unwrap();
-                        (*ip).saddr = vip;
-                        (*ip).daddr = clientinfo.ip;
+                        let clientinfo = match L2C.get(&localinfo) {
+                            Some(v) => v,
+                            None => return Ok(XdpAction::Drop),
+                        };
+                        // _ = printk!("client ip: %x, client port: %d", clientinfo.ip, clientinfo.port);
 
-                        (*tcp).dest = clientinfo.port;
-                        (*tcp).source = vport;
+                        // process eth hdr
+                        let ethh = match ctx.eth_mut() {
+                            Ok(ethhdr) => ethhdr,
+                            Err(_) => return Ok(XdpAction::Drop),
+                        };
+                        process_eth(ethh, (*ethh).h_dest, [0x0, 0x0, 0x0, 0x0, 0x0, 0x01]);
+                        process_l3(iph, vip, clientinfo.ip);
+                        process_l4_tcp(tcph, vport.to_be(), clientinfo.port);
+                        _ = printk!("local tx");
+
                         return Ok(XdpAction::Tx);
                     }
 
-                    if (*ip).saddr == rip && (*tcp).dest != 32987 {
-                        printk!("from rs: dport: %u", (*tcp).dest);
+                    if (*iph).saddr == rip && (*tcph).dest != 32987 && (*tcph).dest != 35555 {
+                        _ = printk!("?????");
+                        _ = printk!("dst ip: %x", (*iph).daddr);
+                        // _ = printk!("lport(44444 be) %x(%u)", lport, lport);
+                        _ = printk!("from rs: dport: %x(%u)", (*tcph).dest, (*tcph).dest);
+                        _ = printk!(
+                            "to_le: %x(%u)",
+                            (*tcph).dest.swap_bytes(),
+                            (*tcph).dest.swap_bytes()
+                        );
                     }
                     return Ok(XdpAction::Pass);
                 }
